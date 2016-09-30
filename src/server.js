f84c68dc5b698876c68b42a42a9112bd620e365f
Eldar Aliyev <eldar.aliyev8@gmail.com>
2016-09-30 13:25:40 +0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+const PORT = 8431;
+
+router.get('/', async ctx => {
+  console.log('success!');
+
+  const a = 1234;
+
+  const b = 4321;
+
+  console.log(a + b);
+
+  ctx.body = {
+    success: true
+  };
+});
+
+app.use(router.routes());
+
+app.listen(PORT, () => {
+  console.log(`Server starteded on http://localhost:${PORT}`);
+});
