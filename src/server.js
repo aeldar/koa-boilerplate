3bdac2b8b3265e61ae53f060b5c9aea5d66fd26f
Eldar Aliyev <eldar.aliyev8@gmail.com>
2016-09-30 16:30:23 +0500

    Add flowtype

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
+// @flow
+
 import Koa from 'koa';
 import Router from 'koa-router';
 
 const app = new Koa();
 const router = new Router();
-const PORT = 8431;
+const PORT: number = 8431;
+
+app.use(async (ctx, next) => {
+  const startTime = Date.now();
+
+  await next();
+
+  const timeToProcess = Date.now() - startTime;
+  const fullUrl = `${ctx.protocol}://${ctx.host}${ctx.originalUrl}`;
+
+  console.log(`${ctx.method} ${fullUrl} - ${timeToProcess}`);
+});
 
 app.use(async (ctx, next) => {
   console.log('before next');
@@ -22,5 +35,5 @@ router.get('/', async ctx => {
 app.use(router.routes());
 
 app.listen(PORT, () => {
-  console.log(`Server starteded on http://localhost:${PORT}`);
+  console.log(`Server started on http://localhost:${PORT}`);
 });
